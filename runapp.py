f04f7a81f051b3c78ae8ab42801b5a5fdfbc7eed
pierfort123 <pierfort123@users.noreply.github.com>
2015-05-17 13:39:41 +0200

    Delete runapp.py

--- runapp.py
+++
@@ -1,9 +0,0 @@
-from paste.deploy import loadapp
-from waitress import serve
-import os
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 6543))
-    app = loadapp('config:runapp.ini', relative_to='.')
-    serve(app, host='0.0.0.0', port=port, expose_tracebacks=True)
